65596b6b8d4721fdea0b5c7bba4f7978eb20916e
smoisset <smoisset@worldremit.com>
2017-05-16 21:41:50 +0200

    Update with Redux

---
+++ src/components/Index.js
@@ -0,0 +1,17 @@
+import Header from './Header/Header';
+import TeslaCar from './TeslaCar/TeslaCar';
+import TeslaClimate from './TeslaClimate/TeslaClimate';
+import TeslaCounter from './TeslaCounter/TeslaCounter';
+import TeslaNotice from './TeslaNotice/TeslaNotice';
+import TeslaStats from './TeslaStats/TeslaStats';
+import TeslaWheels from './TeslaWheels/TeslaWheels';
+
+export {
+    Header,
+    TeslaCar,
+    TeslaClimate,
+    TeslaCounter,
+    TeslaNotice,
+    TeslaStats,
+    TeslaWheels
+}
---
+++ src/components/TeslaStats/TeslaStats.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TeslaStats.css';
+
+const propTypes = {
+  carstats: PropTypes.array
+}
+
+const TeslaStats = (props) => {
+  const listItems = props.carstats.map((stat) => (
+    <li key={stat.model}>
+      <div className={`tesla-stats-icon tesla-stats-icon--${stat.model.toLowerCase()}`}></div>
+      <p>{stat.miles}</p>
+    </li>
+  ));
+  return (
+    <div className="tesla-stats">
+    <ul>
+      {listItems}  
+    </ul>
+  </div>
+  )
+};
+
+TeslaStats.propTypes = propTypes;
+
+export default TeslaStats
